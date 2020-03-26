b9b94a6152eab192ef8d8b13b3271a57d0997ea3
Tomasz Steć <tomeczekstecc@gmail.com>
2020-03-26 19:37:34 +0100

    d

--- js/app.js
+++ js/app.js
@@ -1,56 +1,89 @@
-const operContainer = document.getElementById('oper-container')
-const benContainer = document.getElementById('ben-container')
+const operContainer = document.getElementById('oper-container');
+const benContainer = document.getElementById('ben-container');
 
 const createDocs = (docsArr, type) => {
+  let container;
 
-  let container
+  type === 'oper' ? (container = operContainer) : (container = benContainer);
 
-  type === 'oper' ? container = operContainer : container = benContainer
-
-  container.innerHTML = docsArr.filter(doc => doc.target == type).map(doc => {
-    return (`
-                <div class="doc col-sm-6 col-md-4 slide">
+  container.innerHTML = docsArr
+    .filter(doc => doc.target == type)
+    .map(doc => {
+      let premiereTag;
+      doc.premiereTag === 'true' ? (premiereTag = 'newDoc') : (premiereTag = 'wDoc');
+      console.log(doc.premiereTag);
+      return `
+                <div id = "${premiereTag}"class="doc col-sm-6 col-md-4 slide ${premiereTag}"  >
                 <div class="slide_content">
                     <div>${doc.title}</div>
                     <div class="slide_options">
-                        <a href="ins_pdf/${'[' + doc.ver + ']' + '_' + doc.shortTitle}.pdf" target="_blank"
+                        <a href="app_files/pdf/${'[' +
+                          doc.ver +
+                          ']' +
+                          '_' +
+                          doc.shortTitle +
+                          '_' +
+                          doc.target}.pdf" target="_blank"
                             class="anchor_1">
                             <i class="icon-down-circled2 option_1">Otwórz PDF</i>
                         </a>
 
-                        <a href="ins_pdf/${'[' + doc.ver + ']' + '_' + doc.shortTitle}.zip" download
-                            class="anchor_2">
-                            <i class="icon-file-archive option_2">Pobierz .zip</i>
-                        </a>
 
                     </div>
                 </div>
-                <img alt="strona tytułowa" src="img/${'[' + doc.ver + ']' + '_' + doc.shortTitle}.png">
+                <img alt="strona tytułowa" src="app_files/img/thumbnail.png">
                 <div class="doc_aside">
                     <div class="doc_info_head">${doc.shortTitle.toUpperCase()}</div>
                     <div class="doc_info">
-                        <i class="icon-calendar-check-o"></i>Data przyjęcia: ${doc.publishDate}
+                        <i class="icon-calendar-check-o"></i>Data przyjęcia: ${
+                          doc.publishDate
+                        }
                         <br>
-                        <i class="icon-sort-number-up"></i>Numer wersji: ${doc.ver}
+                        <i class="icon-sort-number-up"></i>Numer wersji: ${
+                          doc.ver
+                        }
                         <br>
-                        <i class="icon-bookmark"></i>Liczba stron: ${doc.pagesCount}
+                        <i class="icon-bookmark"></i>Liczba stron: ${
+                          doc.pagesCount
+                        }
                         <br>
-                        <i class="icon-back-in-time"></i>Poprzednia wersja: ${doc.prevVer}
+                        <i class="icon-back-in-time"></i>Poprzednia wersja: ${
+                          doc.prevVer
+                        }
                     </div>
 
                 </div>
             </div>
 
-`)
-  }).join(' ')
-
-}
-
+`;
+    })
+    .join(' ');
+};
 
 fetch('https://docprovider.netlify.com/.netlify/functions/server', {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
   }
-}).then(res => res.json()).then(res => { createDocs(res.data, 'oper'); createDocs(res.data, 'ben'); console.log(res.data) })
+})
+  .then(res => res.json())
+  .then(res => {
+    createDocs(res.data, 'oper');
+    createDocs(res.data, 'ben');
+    console.log(res.data);
+  });
 
+setTimeout(addItemCss, 500);
+
+function addItemCss() {
+  const newDoc = document.querySelectorAll('.newDoc');
+  newDoc.forEach(item => {
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = '<h3>NOWOŚĆ</h3>';
+  newDiv.id='newItem';
+  console.log(newDoc);
+    item.appendChild(newDiv)
+  });
+
+  // newDoc.appendChild(newDiv);
+}
